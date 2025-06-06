b7bb7182ee52ae9a60e225a4ca552737b5b49cf5
Vu Nguyen <vuthanhnguyen201004@gmail.com>
2025-06-06 10:51:30 -0400

    Added Common Layout and Header

---
+++ blog-app/src/components/Layout/layout.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from './header'
+
+export default function CommonLayout({ children }) {
+    return (
+        <div className='min-h-screen bg-white'>
+            <Header/>
+            {children}
+        </div>
+    )
+}
+
