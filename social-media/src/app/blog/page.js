c8c6fb96cd2cbc3a258d9b56cb58a142684684a9
Vu Nguyen <vuthanhnguyen201004@gmail.com>
2025-06-15 02:38:33 -0400

    set up middle grid blog page

--- social-media/src/app/blog/page.js
+++ social-media/src/app/blog/page.js
@@ -1,12 +1,34 @@
+"use client"
 import React from 'react'
 import CreateNewPost from '@/components/CreateNewPost'
 import OwnerInf from '@/components/OwnerInf'
+import { useAuth } from "@clerk/nextjs";
+import { getUserById } from '@/actions/user';
 
 export default function BlogPage() {
+  const { isSignedIn, userId } = useAuth();
+  const [user, setUser] = React.useState(null);
+
+  React.useEffect(() => {
+    if (!userId) return;
+
+    async function fetchUser() {
+      try {
+        const data = await getUserById({ clerkId: userId });
+        setUser(data);
+      } catch (error) {
+        console.error("Failed to fetch user (Blog Page):", err);
+      }
+    }
+
+    fetchUser();
+
+  }, [userId])
+
   return (
     <div className='grid grid-cols-1 lg:grid-cols-10 gap-6 border border-red-300'>
       <div className='lg:col-span-6 h-[2000px]'>
-        <CreateNewPost />
+        {isSignedIn && (user?.role === "admin") && <CreateNewPost />}
       </div>
       <div className='hidden lg:block lg:col-span-4 sticky top-20'>
         <OwnerInf />
--- social-media/src/components/CreateNewPost.jsx
+++ social-media/src/components/CreateNewPost.jsx
@@ -1,6 +1,9 @@
+"use client"
 import React from 'react'
 
 export default function CreateNewPost() {
+  const [content, setContent] = React.useState("");
+
   return (
     <div>
       Create New post
