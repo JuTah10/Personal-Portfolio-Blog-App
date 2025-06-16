b86f684d21c75b3edb544d48e97b3ff4e0d3e1e9
Vu Nguyen <vuthanhnguyen201004@gmail.com>
2025-06-15 22:54:42 -0400

    add owner social constant file

--- social-media/src/components/OwnerInf.jsx
+++ social-media/src/components/OwnerInf.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
+import { Avatar, AvatarImage } from "./ui/avatar";
+import Link from "next/link";
+
 
 export default function OwnerInf() {
-  return (
-    <div>
-      Owner Information
-    </div>
-  )
+    return (
+        <Card>
+            <CardHeader>
+                <CardTitle>Connect with me at:</CardTitle>
+            </CardHeader>
+            <CardContent>
+                
+            </CardContent>
+        </Card>
+    )
 }
---
+++ social-media/src/constant/OwnerSocial.js
@@ -0,0 +1,25 @@
+import { FaExternalLinkAlt, FaGithub, FaLinkedin } from "react-icons/fa";
+import { MdOutlineEmail } from "react-icons/md";
+
+export const OwnerSocials = [
+    {
+        name: "Website",
+        link: "https://yourwebsite.com",
+        Icon: FaExternalLinkAlt,
+    },
+    {
+        name: "Email",
+        link: "mailto:you@example.com",
+        Icon: MdOutlineEmail,
+    },
+    {
+        name: "LinkedIn",
+        link: "https://linkedin.com/in/yourprofile",
+        Icon: FaLinkedin,
+    },
+    {
+        name: "Github",
+        link: "https://github.com/yourusername",
+        Icon: FaGithub,
+    },
+];
