4b633399a26deac593b74b8b616b9c6078ff83cc
Vu Nguyen <vuthanhnguyen201004@gmail.com>
2025-06-30 00:54:43 -0400

    Add notifications page layout style

--- social-media/src/app/notifications/page.js
+++ social-media/src/app/notifications/page.js
@@ -3,14 +3,41 @@ import React from 'react'
 import { getNotifications } from '@/actions/notifications'
 import { UserLogInContext } from '@/components/UserLogInContextBlog';
 
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuPortal,
+  DropdownMenuSeparator,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
+import { Ellipsis, Check, Settings } from "lucide-react"
+
 export default function NotificationsPage() {
   const [notifications, setNotifications] = React.useState([]);
+  const [readNotifications, setReadNotifications] = React.useState(true);
   const { userInf } = React.useContext(UserLogInContext);
 
 
   React.useEffect(() => {
     async function fetchNotifications() {
-      const data = await getNotifications({ receiverId: userInf.id }) 
+      const data = await getNotifications({ receiverId: userInf.id })
       setNotifications(data)
 
     }
@@ -19,9 +46,63 @@ export default function NotificationsPage() {
   }, [])
 
 
+
   return (
-    <div>
-      
+    <div className='w-full flex justify-center items-center'>
+      <Card className="w-[80%]">
+        <CardHeader>
+          <CardTitle>
+            Notifications
+          </CardTitle>
+          <CardDescription
+            className="my-2"
+          >
+            <Button
+              variant={`${readNotifications ? "secondary" : "ghost"}`}
+              className="rounded-3xl cursor-pointer"
+               onClick={()=>setReadNotifications(true)}
+            >
+              All
+            </Button>
+            <Button
+              variant={`${!readNotifications ? "secondary" : "ghost"}`}
+              className="rounded-3xl cursor-pointer"
+              onClick={()=>setReadNotifications(false)}
+            >
+              Unread
+            </Button>
+          </CardDescription>
+          <CardAction>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button
+                  variant="ghost"
+                  className="rounded-3xl cursor-pointer"
+                >
+                  <Ellipsis />
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="w-56" align="end">
+                <DropdownMenuGroup>
+                  <DropdownMenuItem className="cursor-pointer">
+                    <Check /> Mark all as read
+                  </DropdownMenuItem>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem disabled>
+                    <Settings /> Notification Settings
+                  </DropdownMenuItem>
+                  <DropdownMenuSeparator />
+                </DropdownMenuGroup>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </CardAction>
+        </CardHeader>
+        <CardContent>
+
+        </CardContent>
+     
+      </Card>
     </div>
+
   )
 }
