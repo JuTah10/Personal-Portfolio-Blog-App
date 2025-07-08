dd05b56f692d3405caf6ecbd6bd4c419227302f0
Vu Nguyen <vuthanhnguyen201004@gmail.com>
2025-07-07 23:52:00 -0400

    Finished Introduction HomePage

--- social-media/src/components/Introduction.jsx
+++ social-media/src/components/Introduction.jsx
@@ -1,13 +1,35 @@
 "use client"
 import React from 'react'
 import { TypeAnimation } from 'react-type-animation';
+import { motion } from 'framer-motion';
+
+import { Github, Linkedin, Mail } from 'lucide-react';
 
 
 export default function Introduction() {
+    const containerVariants = {
+        hidden: {},
+        visible: {
+            transition: {
+                delayChildren: 0.5,
+                staggerChildren: 0.2,
+            },
+        },
+    };
+
+    const itemVariants = {
+        hidden: { x: 50, opacity: 0 },
+        visible: { x: 0, opacity: 1 },
+    };
     return (
         <div className="flex flex-col-reverse items-center justify-evenly md:flex-row md:gap-12 h-full ">
             {/* Text Content */}
-            <div className="text-center md:text-left max-w-xl space-y-4">
+            <motion.div
+                className="text-center md:text-left max-w-xl space-y-4"
+                initial={{ opacity: 0, x: 100, y: 100, filter: 'blur(8px)', scale: 0.7 }}
+                animate={{ opacity: 1, x: 0, y: 0, filter: 'blur(0px)', scale: 1 }}
+                transition={{ duration: 0.4, ease: 'easeOut' }}
+            >
                 <h1 className="text-4xl font-bold">
                     <TypeAnimation
                         sequence={[
@@ -28,19 +50,36 @@ export default function Introduction() {
                 <p>
                     I'm especially fascinated by large-scale, high-impact products.
                 </p>
-                <div>
-                    <p>Socials</p>
-                </div>
-            </div>
+                <motion.div
+                    className="flex gap-4"
+                    variants={containerVariants}
+                    initial="hidden"
+                    animate="visible"
+                >
+                    {[<Github />, <Linkedin />, <Mail />].map((Icon, index) => (
+                        <motion.div key={index} variants={itemVariants}>
+                            {Icon}
+                        </motion.div>
+                    ))}
+                </motion.div>
+            </motion.div>
 
             {/* Image */}
-            <div className="w-60 h-60 rounded-full overflow-hidden border">
+            <motion.div
+                initial={{ opacity: 0, x: -100, y: 100, filter: 'blur(8px)', scale: 0.7 }}
+                animate={{ opacity: 1, x: 0, y: 0, filter: 'blur(0px)', scale: 1 }}
+                transition={{ duration: 0.4, ease: 'easeOut' }}
+                className="w-60 h-60 rounded-full overflow-hidden border"
+            >
                 <img
                     src="https://avatars.githubusercontent.com/u/126214032?v=4"
                     alt="Vu's avatar"
                     className="object-cover w-full h-full"
                 />
-            </div>
+            </motion.div>
+
+
+
         </div>
     )
 }
