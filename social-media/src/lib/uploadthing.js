4bea9135a280e0a3baa941196ed519ef83d7a14f
Vu Nguyen <vuthanhnguyen201004@gmail.com>
2025-07-11 17:53:43 -0400

    WIP

--- social-media/src/lib/uploadthing.js
+++
@@ -1,5 +0,0 @@
-import { generateUploadDropzone, generateUploadButton } from "@uploadthing/react";
-
-// You can skip <OurFileRouter> in JS
-export const UploadButton = generateUploadButton();
-export const UploadDropzone = generateUploadDropzone();
