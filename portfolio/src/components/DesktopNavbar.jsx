71b95926e8eecb477a02716861a87f62ee03992f
Vu Nguyen <vuthanhnguyen201004@gmail.com>
2025-07-12 20:05:29 -0400

    Set up command layout

--- portfolio/src/components/DesktopNavbar.jsx
+++ portfolio/src/components/DesktopNavbar.jsx
@@ -1,4 +1,5 @@
 "use client"
+import React from "react";
 import { BellIcon, HomeIcon, UserIcon, NotebookPen, FolderDot, LogOutIcon } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
@@ -6,13 +7,61 @@ import { SignInButton, UserButton } from "@clerk/nextjs";
 import { ModeToggle } from "@/components/ui/ModeToggle";
 
 
+import {
+    Calculator,
+    Calendar,
+    CreditCard,
+    Settings,
+    Smile,
+    User,
+} from "lucide-react"
+
+import {
+    CommandDialog,
+    CommandEmpty,
+    CommandGroup,
+    CommandInput,
+    CommandItem,
+    CommandList,
+    CommandSeparator,
+    CommandShortcut,
+} from "@/components/ui/command"
+
+
 function DesktopNavbar({ user }) {
+    const [open, setOpen] = React.useState(false);
+
+
+    React.useEffect(() => {
+        function down(e) {
+            if (e.key === "k" && (e.ctrlKey)) {
+                e.preventDefault()
+                setOpen((open) => !open)
+            }
+        }
+        document.addEventListener("keydown", down)
+        return () => document.removeEventListener("keydown", down)
+    }, [])
+
     return (
         <div className="hidden md:flex items-center space-x-4">
             <ModeToggle />
 
-            <Button variant="secondary" className="flex items-center gap-2 cursor-pointer" asChild>
-                <span className="hidden lg:inline">Command</span>
+            <Button
+                onClick={() => setOpen((open) => !open)}
+                variant="secondary" className="flex items-center gap-2 cursor-pointer"
+                asChild
+            >
+                <span className="hidden lg:inline">
+                    Press{" "}
+                    <kbd className="bg-muted text-muted-foreground pointer-events-none inline-flex h-5 items-center gap-1 rounded border px-1.5 font-mono text-[10px] font-medium opacity-100 select-none">
+                        <span className="text-xs">Ctrl</span>
+                    </kbd>
+                    <span className="mx-1">+</span>
+                    <kbd className="bg-muted text-muted-foreground pointer-events-none inline-flex h-5 items-center gap-1 rounded border px-1.5 font-mono text-[10px] font-medium opacity-100 select-none">
+                        <span className="text-xs">K</span>
+                    </kbd>
+                </span>
             </Button>
 
             <Button variant="ghost" className="flex items-center gap-2" asChild>
@@ -33,6 +82,44 @@ function DesktopNavbar({ user }) {
                     <span className="hidden xl:inline">Blog</span>
                 </Link>
             </Button>
+            <CommandDialog open={open} onOpenChange={setOpen}>
+                <CommandInput placeholder="Type a command or search..." />
+                <CommandList>
+                    <CommandEmpty>No results found.</CommandEmpty>
+                    <CommandGroup heading="Suggestions">
+                        <CommandItem>
+                            <Calendar />
+                            <span>Calendar</span>
+                        </CommandItem>
+                        <CommandItem>
+                            <Smile />
+                            <span>Search Emoji</span>
+                        </CommandItem>
+                        <CommandItem>
+                            <Calculator />
+                            <span>Calculator</span>
+                        </CommandItem>
+                    </CommandGroup>
+                    <CommandSeparator />
+                    <CommandGroup heading="Settings">
+                        <CommandItem>
+                            <User />
+                            <span>Profile</span>
+                            <CommandShortcut>⌘P</CommandShortcut>
+                        </CommandItem>
+                        <CommandItem>
+                            <CreditCard />
+                            <span>Billing</span>
+                            <CommandShortcut>⌘B</CommandShortcut>
+                        </CommandItem>
+                        <CommandItem>
+                            <Settings />
+                            <span>Settings</span>
+                            <CommandShortcut>⌘S</CommandShortcut>
+                        </CommandItem>
+                    </CommandGroup>
+                </CommandList>
+            </CommandDialog>
 
             {user ? (
                 <>
