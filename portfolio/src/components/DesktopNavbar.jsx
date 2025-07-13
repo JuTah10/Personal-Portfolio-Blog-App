c42596e5da9c10d6fc3fcfc9bbce094d5d334e65
Vu Nguyen <vuthanhnguyen201004@gmail.com>
2025-07-12 20:47:11 -0400

    Detect user hold ctrl button

--- portfolio/src/components/DesktopNavbar.jsx
+++ portfolio/src/components/DesktopNavbar.jsx
@@ -30,17 +30,34 @@ import {
 
 function DesktopNavbar({ user }) {
     const [open, setOpen] = React.useState(false);
+    const [hideCtrlKey, setHideCtrKey] = React.useState(false);
+    const [hideShiftKey, setShiftCtrKey] = React.useState(false);
 
 
     React.useEffect(() => {
         function down(e) {
+
+            if (e.ctrlKey) {
+                console.log("aoshjdoasd")
+                setHideCtrKey(true);
+
+            }
             if (e.key === "k" && (e.ctrlKey)) {
                 e.preventDefault()
                 setOpen((open) => !open)
             }
         }
+        function up(e) {
+            if (e.key === "Control") {
+                setHideCtrKey(false);
+            }
+        }
         document.addEventListener("keydown", down)
-        return () => document.removeEventListener("keydown", down)
+        document.addEventListener("keyup", up);
+        return () => {
+            document.removeEventListener("keydown", down);
+            document.removeEventListener("keyup", up);
+        };
     }, [])
 
     return (
@@ -54,9 +71,13 @@ function DesktopNavbar({ user }) {
             >
                 <span className="hidden lg:inline">
                     Press{" "}
-                    <kbd className="bg-muted text-muted-foreground pointer-events-none inline-flex h-5 items-center gap-1 rounded border px-1.5 font-mono text-[10px] font-medium opacity-100 select-none">
+
+
+                    <kbd className={`${hideCtrlKey && "invisible"} bg-muted text-muted-foreground pointer-events-none inline-flex h-5 items-center gap-1 rounded border px-1.5 font-mono text-[10px] font-medium opacity-100 select-none`}>
                         <span className="text-xs">Ctrl</span>
                     </kbd>
+
+
                     <span className="mx-1">+</span>
                     <kbd className="bg-muted text-muted-foreground pointer-events-none inline-flex h-5 items-center gap-1 rounded border px-1.5 font-mono text-[10px] font-medium opacity-100 select-none">
                         <span className="text-xs">K</span>
